df5ee78d9ad54ccd934536c5a93582d600af5a08
kei178 <keisuke.inaba.cpa@gmail.com>
2021-05-13 20:58:40 -0500

    Add reviews controller

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -1,5 +1,29 @@
 module Api
   module V1
-    class ReviewsController < ApplicationController; end
+    class ReviewsController < ApplicationController
+      def create
+        review = Review.new(review_params)
+        if review.save
+          render json: ReviewSerializer.new(review)
+        else
+          render json: { error: review.errors.full_messages }, status: 422
+        end
+      end
+
+      def destroy
+        review = Review.find(params[:id])
+        if review.destroy
+          head :no_content
+        else
+          render json: { error: review.errors.full_messages }, status: 422
+        end
+      end
+
+      private
+
+      def review_params
+        params.require(:review).permit(%i[title description score airline_id])
+      end
+    end
   end
 end
