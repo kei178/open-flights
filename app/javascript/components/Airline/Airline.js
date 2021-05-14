7b90546269015694da58a3f1919cdcd40703e3cc
kei178 <keisuke.inaba.cpa@gmail.com>
2021-05-14 10:04:21 -0500

    Add react setup

---
+++ app/javascript/components/Airline/Airline.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Airline = () => {
+  return <div>This is the show airline</div>;
+};
+
+export default Airline;
---
+++ app/javascript/components/Airlines/Airlines.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Airlines = () => {
+  return <div>This is the index airlines</div>;
+};
+
+export default Airlines;
